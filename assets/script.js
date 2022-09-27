089b6e20dd617d478def81c641d2d59e987fd422
mjkonkel <mitchell.konkel@gmail.com>
2022-09-26 22:14:37 -0500

    cleaning up code

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,45 @@
-
 var startButton = document.querySelector(".start-button");
 var timerEl = document.querySelector(".timer");
-var timeLeft = 50;
+var timeLeft = 60;
 
 // variable containing the questions
 var questions = [
     {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
-    answer: "3. Alerts", 
+        title: "Commonly used data types DO NOT include:",
+        choices: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+        answer: "3. Alerts",
     },
     {
-    title: "The condition in and if / else statement is enclosed within ____.",
-    choices: ["1. Quotes", "2. Curly Brackets", "3. Parentheses", "4. Square Brackets"],
-    answer: "3. Parentheses", 
+        title: "The condition in and if / else statement is enclosed within ____.",
+        choices: ["1. Quotes", "2. Curly Brackets", "3. Parentheses", "4. Square Brackets"],
+        answer: "3. Parentheses",
     },
     {
-    title: "Arrays in JavaScript can be used to store ____.",
-    choices: ["1. Numbers and Strings", "2. Other Arrays", "3. Booleans", "4. All of the Above"],
-    answer: "3. All of the Above", 
+        title: "Arrays in JavaScript can be used to store ____.",
+        choices: ["1. Numbers and Strings", "2. Other Arrays", "3. Booleans", "4. All of the Above"],
+        answer: "3. All of the Above",
     },
     {
-    title: "String values must be enclosed within ____ when being assigned to variables.",
-    choices: ["1. Commas", "2. Curly Brackets", "3. Quotes", "4. Parentheses"],
-    answer: "3. Quotes", 
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["1. Commas", "2. Curly Brackets", "3. Quotes", "4. Parentheses"],
+        answer: "3. Quotes",
     },
     {
-    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choices: ["1. JavaScript", "2. Terminal / Bash", "3. For Loops", "4. Console Log"],
-    answer: "4. Console Log", 
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["1. JavaScript", "2. Terminal / Bash", "3. For Loops", "4. Console Log"],
+        answer: "4. Console Log",
     },
 ]
 
 // function for ending the quiz
 function endQuiz() {
+    clearInterval(startTimer)
     questionDiv.innerHTML = '';
     var h3El = document.createElement('h3')
     h3El.textContent = 'All done!'
 
     var pDiv = document.createElement('div')
     pDiv.textContent = 'Your final score is ' + timeLeft
-
-    
 
     questionDiv.append(h3El, pDiv)
 }
@@ -51,11 +49,10 @@ function checkAnswer() {
     if (this.value !== questions[indexNumber].answer) {
         timeLeft -= 10;
 
-    // if (timeLeft < 0) {
-    //     timeLeft = 0
-    //     clearInterval(startTimer)
-    //     endQuiz()
-    // }
+        if (timeLeft < 0) {
+            timeLeft = 0
+            endQuiz()
+        }
     }
 }
 
@@ -65,24 +62,22 @@ function countdown() {
 
     startTimer = setInterval(function () {
         console.log(timeLeft)
-        if (timeLeft >= 0) {
-        timerEl.textContent = "Time: " + timeLeft;
-        timeLeft--;
-        } 
+        if (timeLeft > 0) {
+            timerEl.textContent = "Time: " + timeLeft;
+            timeLeft--;
+        }
         if (timeLeft === 0) {
             console.log("Time's up!")
-            clearInterval(startTimer)
+            // clearInterval(startTimer)
             timerEl.textContent = "Time's up!";
             endQuiz()
-        } 
-        
+        }
     }, 1000);
 
 }
 
 // function for showing the questions
 var questionDiv = document.getElementById('questionDiv')
-
 var indexNumber = 0
 
 function start() {
@@ -108,21 +103,12 @@ function start() {
             checkAnswer()
         })
         buttonDiv.append(btnEl)
-
     }
-
-
     questionDiv.append(h3El, buttonDiv)
-
     document.querySelector(".intro").classList.add("hidden")
 }
 
-
-
-
-
-
-
+// to start the quiz
 startButton.addEventListener("click", function () {
     countdown()
     start()
